22bec5391d9d3cfd9cd88cdac67ff432bdbd3e56
chriszarate <chris@zarate.org>
2015-06-10 01:36:44 -0400

    Use Ansible 1.9.0.1.

--- packer/scripts/ansible-ubuntu.sh
+++ packer/scripts/ansible-ubuntu.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-sudo apt-get -y update
-sudo apt-get -y install software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get -y update
-sudo apt-get -y install ansible
+
+# AWS accommodation
+sleep 15
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Ansible 1.9.1 is broken; use pip to install 1.9.0.1.
+# sudo apt-get install -y software-properties-common
+# sudo apt-add-repository -y ppa:ansible/ansible
+# sudo apt-get update
+# sudo apt-get -y install ansible
+sudo apt-get install -y python-dev
+curl -so /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
+sudo python /tmp/get-pip.py
+sudo -H pip install ansible==1.9.0.1
