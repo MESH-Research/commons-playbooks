a7bed9e1466d8dd6ae6c2342ee04695256f80694
chriszarate <chris@zarate.org>
2015-06-10 18:13:21 -0400

    Run scripts with sudo.

--- packer/scripts/ansible-ubuntu.sh
+++ packer/scripts/ansible-ubuntu.sh
@@ -3,15 +3,15 @@
 # AWS accommodation
 sleep 15
 
-sudo apt-get update
-sudo apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
 # Ansible 1.9.1 is broken; use pip to install 1.9.0.1.
-# sudo apt-get install -y software-properties-common
-# sudo apt-add-repository -y ppa:ansible/ansible
-# sudo apt-get update
-# sudo apt-get -y install ansible
-sudo apt-get install -y python-dev
+# apt-get install -y software-properties-common
+# apt-add-repository -y ppa:ansible/ansible
+# apt-get update
+# apt-get -y install ansible
+apt-get install -y python-dev
 curl -so /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
-sudo python /tmp/get-pip.py
-sudo -H pip install ansible==1.9.0.1
+python /tmp/get-pip.py
+pip install ansible==1.9.0.1
--- packer/scripts/sudo-passwd.sh
+++ packer/scripts/sudo-passwd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# Remove sudo password requirement.
-echo 'vagrant' | sudo -S sed -i -E 's/^%sudo.+/%sudo ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+# Remove sudo password.
+sed -i -E 's/^%sudo.+/%sudo ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
